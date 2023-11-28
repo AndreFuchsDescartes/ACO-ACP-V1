55e102fd9afe887e0debd290b124d34022d151ad
AndreFuchsDescartes <andfuna@gmail.com>
2023-11-28 19:26:29 +0100

    added save-functionality defined functions for reuse

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ import configparser
 
 # Initialize Variables
 filepath = "C:\\Users\\User\\Documents\\ACP.ini"
+parameterValueList = configparser.ConfigParser()
+
+# Define Functions
+def updateParameterValues(filepath):
+    with open(filepath) as stream:
+        parameterValueList.read_string("[top]\n" + stream.read()) # add 'top' as dummy header, since the file doesn'come with one
+    
+def applyParameterValues():
+    with open(filepath, 'w') as configfile:
+        parameterValueList.write(configfile)
 
 ## Read from File
-
-config = configparser.ConfigParser()
-with open(filepath) as stream:
-    config.read_string("[top]\n" + stream.read()) # add 'top' as dummy header, since the file doesn'come with one
-print(config['top']['PlayableCharacterIndex'])
+updateParameterValues(filepath)
+print(parameterValueList['top']['PlayableCharacterIndex'])
 
 # Update Values of Variables based on File
+parameterValueList.set('top','PlayableCharacterIndex','1')
 
 # Edit Values of Variables based on File
 
 # Apply Changes
+applyParameterValues()
