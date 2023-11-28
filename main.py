24f73e742a20d55022674a70368dc07929551f4b
AndreFuchsDescartes <andfuna@gmail.com>
2023-11-28 19:11:05 +0100

    added first Config Read Functionality

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 # Main File
+import configparser
 
 # Initialize Variables
+filepath = "C:\\Users\\User\\Documents\\ACP.ini"
 
-# Read from File
+## Read from File
 
-# update Values of Variables based on File
+config = configparser.ConfigParser()
+with open(filepath) as stream:
+    config.read_string("[top]\n" + stream.read()) # add 'top' as dummy header, since the file doesn'come with one
+print(config['top']['PlayableCharacterIndex'])
+
+# Update Values of Variables based on File
 
 # Edit Values of Variables based on File
 
