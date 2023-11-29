0243f1e06844422fc1593bff34cfa31b23516ab6
AndreFuchsDescartes <andfuna@gmail.com>
2023-11-29 12:31:49 +0100

    replaced Configparser with self-made module

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 # Main File
-import configparser
+
+from parameter_list import ParameterList
 
 # Initialize Variables
 filepath = "C:\\Users\\User\\Documents\\ACP.ini"
-parameterValueList = configparser.ConfigParser()
+parameters = ParameterList()
 
 # Define Functions
 def updateParameterValues(filepath):
-    with open(filepath) as stream:
-        parameterValueList.read_string("[top]\n" + stream.read()) # add 'top' as dummy header, since the file doesn'come with one
+    parameters.parse(filepath)
     
 def applyParameterValues():
-    with open(filepath, 'w') as configfile:
-        parameterValueList.write(configfile)
-
+    pass
+ 
 ## Read from File
 updateParameterValues(filepath)
-print(parameterValueList['top']['PlayableCharacterIndex'])
+
 
 # Update Values of Variables based on File
-parameterValueList.set('top','PlayableCharacterIndex','1')
+
 
 # Edit Values of Variables based on File
+parameters.set('DamageInSandstorm','0')
+print(parameters.list)
 
 # Apply Changes
 applyParameterValues()
---
+++ parameter_list.py
@@ -0,0 +1,32 @@
+class ParameterList:
+    import os.path
+
+    def __init__(self) -> None:
+        self.list = []
+        pass
+
+    def parse(self, filepath):
+
+        if not self.os.path.isfile(filepath):
+            print('File does not exist.')
+        else:
+            # Open the file and read its content.
+            with open(filepath) as f:
+                content = f.read().splitlines()
+
+            # Display the file's content line by line.
+            for line in content:
+               line = line.replace('= ','')
+               self.list.append(line.split())
+               
+        print(self.list,"\n")
+        pass
+    
+    def set(self,parameter, value):
+        for i, sublist in enumerate(self.list):
+            if parameter in sublist:
+                self.list[i][1] = value
+        pass
+
+
+
